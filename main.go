c4c443680a93fa44ac538ef0aad4685ded6751af
Yiğit Sadıç <yigitsadic@gmail.com>
2021-09-18 16:03:27 +0300

    updated for connecting to database

--- main.go
+++ main.go
@@ -3,15 +3,20 @@ package main
 import (
 	"database/sql"
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-migrate/migrate/v4"
 	"github.com/yigitsadic/listele/database"
 	"github.com/yigitsadic/listele/handlers"
 	"log"
 
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/lib/pq"
 )
 
 func main() {
-	db, err := sql.Open("postgres", "postgres://myexampleuser:myexample@localhost:6500/listele?sslmode=disable")
+	dataSource := "postgres://myexampleuser:myexample@localhost:6500/listele?sslmode=disable"
+
+	db, err := sql.Open("postgres", dataSource)
 	if err != nil {
 		log.Fatalf("unable to open connection due to=%q", err)
 	}
@@ -20,11 +25,22 @@ func main() {
 		log.Fatal("unable to ping database")
 	}
 
+	m, err := migrate.New("file://db/migrations", dataSource)
+	if err != nil {
+		log.Fatal("unable to run migrations due to ", err)
+	}
+
+	err = m.Up()
+
+	if err != nil && err != migrate.ErrNoChange {
+		log.Fatal("error occurred during execution of migrations ", err)
+	}
+
 	repo := database.PeopleRepository{Database: db}
 
 	app := fiber.New()
 
-	app.Get("/hello", handlers.HandleList(&repo))
+	app.Get("/", handlers.HandleList(&repo))
 
 	log.Fatalln(app.Listen(":3035"))
 }
