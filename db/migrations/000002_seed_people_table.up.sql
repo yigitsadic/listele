e7f68feb14d1ada08e18ea7fbcc429ce8f37cd55
Yiğit Sadıç <yigitsadic@gmail.com>
2021-09-18 13:07:27 +0300

    added seed migration

---
+++ db/migrations/000002_seed_people_table.up.sql
@@ -0,0 +1,4 @@
+INSERT INTO people(fullname) VALUES ('John Doe');
+INSERT INTO people(fullname) VALUES ('Aida Bugg');
+INSERT INTO people(fullname) VALUES ('Maureen Biologist');
+INSERT INTO people(fullname) VALUES ('Allie Grater');
