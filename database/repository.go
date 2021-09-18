dfeaaa6576226f30f209196660298b52049c7335
Yiğit Sadıç <yigitsadic@gmail.com>
2021-09-18 16:02:41 +0300

    added comment lines for interface

--- database/repository.go
+++ database/repository.go
@@ -1,9 +1,11 @@
 package database
 
+// Person represents database row for people table.
 type Person struct {
 	FullName string `json:"full_name"`
 }
 
+// Repository is an interface for communicating between handler and database
 type Repository interface {
 	FindAll() ([]Person, error)
 }
