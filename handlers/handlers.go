d24064cff4ec995e4ae8f6fcdc7001ec6e78ee01
Yiğit Sadıç <yigitsadic@gmail.com>
2021-09-18 15:56:42 +0300

    added comment line

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -5,6 +5,7 @@ import (
 	"github.com/yigitsadic/listele/database"
 )
 
+// HandleList handles listing data taken from repository.
 func HandleList(repo database.Repository) func(ctx *fiber.Ctx) error {
 	return func(ctx *fiber.Ctx) error {
 		people, err := repo.FindAll()
